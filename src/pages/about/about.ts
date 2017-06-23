aa91468150f6261f2b6b45103841545950776c3c
Eli Joesph Bradley <elijos03@gmail.com>
2017-06-23 09:48:08 -0500

    MAKE ITWOKR AYYAYA

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -14,20 +14,20 @@ import { ParticleProvider } from '../../providers/particle/particle';
   templateUrl: 'about.html',
 })
 export class AboutPage {
-  gyro?: Vector;
-  initialGyro?: Vector;
+  public currentGyro?: Vector;
+  public initialGyro?: Vector;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public particle: ParticleProvider) {
   }
 
-  ionViewDidLoad() {
+  async ionViewDidLoad() {
     console.log('ionViewDidLoad AboutPage');
 
     if (this.particle.device) {
-      let initialGyroX = await particle.getVariable("gyroX");
-      let initialGyroY = await particle.getVariable("gyroY");
-      let initialGyroZ = await particle.getVariable("gyroZ");
-      this.initialGyro = new Vector(initialGyroX, initialGyroY, initialGyroZ);
+      let initialGyroX = await this.particle.getVariable("gyroX") as number;
+      let initialGyroY = await this.particle.getVariable("gyroY") as number;
+      let initialGyroZ = await this.particle.getVariable("gyroZ") as number;
+      this.initialGyro = new Vector(5, 7, 4); // CHEESE IT!!!
       this.currentGyro = new Vector(initialGyroX, initialGyroY, initialGyroZ);
 
       this.particle.pollVariable("gyroX").subscribe(
@@ -72,7 +72,15 @@ class Vector {
     return Math.sqrt(this.x ** 2 + this.y ** 2 + this.z ** 2);
   }
 
-  angleDifferenceFrom(other: Vector) {
-    return Math.acos(dot(other) / (magnitude - other.magnitude));
+  radiansFrom(other: Vector) {
+    return Math.acos(this.dot(other) / (this.magnitude * other.magnitude));
+  }
+
+  degreesFrom(other: Vector) {
+    return this.radiansFrom(other) * 360 / Math.PI;
+  }
+
+  toString() {
+    return `(${this.x}, ${this.y}, ${this.z})`;
   }
 }
